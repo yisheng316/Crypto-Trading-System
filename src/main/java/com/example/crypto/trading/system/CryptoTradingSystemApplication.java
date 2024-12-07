ea296aa2795e5bdd09bfa1431147135f7d5c025a
yisheng316 <yeoyisheng1997@outlook.com>
2024-12-07 14:01:22 +0800

    Add Maven project files

---
+++ src/main/java/com/example/crypto/trading/system/CryptoTradingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.crypto.trading.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTradingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTradingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crypto/trading/system/CryptoTradingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crypto.trading.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTradingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
