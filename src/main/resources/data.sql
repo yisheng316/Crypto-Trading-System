d6b2bf88b91d1be407f6fa42c80d6042b111c994
yisheng316 <yeoyisheng1997@outlook.com>
2024-12-07 16:52:40 +0800

    Create initial database script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO wallet (user_id, currency, balance) VALUES (1, 'USDT', 50000.00);
+INSERT INTO wallet (user_id, currency, balance) VALUES (1, 'BTC', 0.00);
+INSERT INTO wallet (user_id, currency, balance) VALUES (1, 'ETH', 0.00);
